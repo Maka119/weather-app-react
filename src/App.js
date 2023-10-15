336b1cbfee849ca1836a0cd2f23f85a4716ee8df
Maka119 <132913072+Maka119@users.noreply.github.com>
2023-10-15 18:12:34 +0200

    js code updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Weather from "./weather";
+import React from "react";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search Engine</h1>
+      <Weather />
+    </div>
+  );
+}
